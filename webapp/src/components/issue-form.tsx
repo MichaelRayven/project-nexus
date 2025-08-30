7814c1ba715d8b650708629622e7c4d6355f116c
Mikhail Zinakov <mihailzinakovvv@gmail.com>
2025-08-31 01:31:56 +0700

    style and locale

--- webapp/src/components/issue-form.tsx
+++ webapp/src/components/issue-form.tsx
@@ -31,6 +31,7 @@ import { AddSubjectDialog } from "./add-subject-dialog";
 import { AddTeacherDialog } from "./add-teacher-dialog";
 import { format } from "date-fns"
 import { Calendar } from "./ui/calendar";
+import { ru } from "date-fns/locale";
 
 const formSchema = z.object({
   title: z
@@ -287,12 +288,12 @@ export function IssueForm() {
                     <Button
                       variant={"outline"}
                       className={cn(
-                        "w-[240px] pl-3 text-left font-normal",
+                        "pl-3 text-left font-normal",
                         !field.value && "text-muted-foreground"
                       )}
                     >
                       {field.value ? (
-                        format(field.value, "PPP")
+                        format(field.value, "PPP", { locale: ru })
                       ) : (
                         <span>Выберите дату</span>
                       )}
@@ -300,11 +301,12 @@ export function IssueForm() {
                     </Button>
                   </FormControl>
                 </PopoverTrigger>
-                <PopoverContent className="w-auto p-0" align="start">
+                <PopoverContent className="w-auto p-0" align="center">
                   <Calendar
                     mode="single"
                     selected={field.value}
                     onSelect={field.onChange}
+                    locale={ru}
                     disabled={(date) =>
                       date < new Date()
                     }
