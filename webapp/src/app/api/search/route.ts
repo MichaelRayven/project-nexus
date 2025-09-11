08f4364020a9e89b26c5cca5e6f6fa03730d06c4
MichaelRayven <themikerayven@gmail.com>
2025-09-12 01:13:24 +0700

    fixed file structure

--- webapp/src/app/api/search/route.ts
+++ webapp/src/app/api/search/route.ts
@@ -1,7 +1,18 @@
-import { source } from '@/lib/source';
-import { createFromSource } from 'fumadocs-core/search/server';
+import { source } from "@/lib/source";
+import { createFromSource } from "fumadocs-core/search/server";
 
 export const { GET } = createFromSource(source, {
   // https://docs.orama.com/docs/orama-js/supported-languages
-  language: 'russian',
+  language: "russian",
+  buildIndex(page) {
+    console.log(page);
+    return {
+      title: page.data.title,
+      description: page.data.description,
+      url: page.url,
+      id: page.url,
+      structuredData: page.data.structuredData,
+      tag: page.slugs[0],
+    };
+  },
 });
